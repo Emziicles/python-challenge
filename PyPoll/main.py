8770d6f2f58dc5d60303a30f05d6b0e4cefa96e9
Emma Smith <e.l.smith93@outlook.com>
2021-11-14 00:46:02 +0000

    Update on PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,33 +1,57 @@
 # declare dependencies
+
 import os
 import csv
 
-# path to collect data from resources folder
+# create file path for csv
 
 election_data = os.path.join("resources", "election_data.csv")
 
-# set variables
-
-
-total_votes = 0
-candidate_list = []
-candidate_votes = {}
-winner_count = 0
-
-
-# open csv
-
-with open(election_data) as csv_file:
+# read the csv file
+with open (election_data) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
 
 # read header row
     csv_header = next(csv_reader)
 
-# go through the rows
+# declare variables
+
+    total_votes = 0
+    candidate_list = []
+    candidate_votes = {}
+    winner_count = 0
+
+
     for row in csv_reader:
         total_votes += 1
-        candidate_name = row[2]
-    print(candidate_name)
+
+        if row[2] not in candidate_votes.keys():
+            candidate_votes[row[2]] = 1
+        else:
+            candidate_votes[row[2]] += 1
+
+print("Election Results")
+print("----------------------------")
+print(f"Total number of votes: {[total_votes]}")
+print("----------------------------")
+
+candidates_info = ""
+for candidates in candidate_votes.keys():
+    candidates_info = '\n'.join([candidates_info, candidates + " {:.2%}".format(candidate_votes[candidates] / total_votes) + "("+ str(candidate_votes[candidates])+ ")"])
+print(candidates_info)
+
+
+winner = max(candidate_votes, key=candidate_votes.get)
+
+print("----------------------------")
+print(f"Winner: {[winner]}")
+print("----------------------------")
+
+# write into new text file
+
+with open("Poll_Export.txt", "w") as export_file:
+    export_file.write()
+
 
 
 
